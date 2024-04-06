e072d8df79bad6cd3bd91127dae18413fe09fcbc
akazicin <akazicin@gmail.com>
2024-04-06 19:14:00 +0300

    Homework String

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //task1
+
+        var firsName = "Ivan";
+        var middName = "Ivanovich";
+        var lastName = " Ivanov";
+        var fullName = firsName + " " + middName + " " + lastName;
+        System.out.println(fullName);
+
+        //task2
+
+        System.out.println(fullName.toUpperCase());
+
+        //task3
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "e");
+        System.out.println(fullName);
     }
 }
